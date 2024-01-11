8e1c56290ad15ac5ed41b3bafadbd96ed7c0c994
Hernan Brando - ZENI <hbrando@zeni.com.ar>
2024-01-11 11:10:42 -0300

    session 11

---
+++ 07-ob-springboot-rest-datajpa_testing_deploy/src/main/java/com/example/obspringbootrestdatajpa/entities/Book.java
@@ -0,0 +1,102 @@
+package com.example.obspringbootrestdatajpa.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name="books")
+public class Book {
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private Double price;
+    private LocalDate releaseDate;
+    private Boolean online;
+    //constructores
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, Integer pages, Double price, LocalDate releaseDate, Boolean online) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.price = price;
+        this.releaseDate = releaseDate;
+        this.online = online;
+    }
+    //getter y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    //to string, por ahora no.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(pages, book.pages) && Objects.equals(price, book.price) && Objects.equals(releaseDate, book.releaseDate) && Objects.equals(online, book.online);
+    }
+
+}
