f1a5fc8333f3284e0c587b784999c01e7309e2d7
Hernan Brando - ZENI <hbrando@zeni.com.ar>
2024-01-13 22:09:40 -0300

    para hacer deploy

--- 04-ob-springboot-rest-datajpa/src/main/java/com/example/obspringbootrestdatajpa/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.obspringbootrestdatajpa.repositories;
-
-import com.example.obspringbootrestdatajpa.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book,Long> {
-}
